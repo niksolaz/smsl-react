cd518152f1ed490999b7dea7bb2b5d688b8b9149
niksolaz <solazzo.nicola@gmail.com>
2015-10-29 18:56:29 +0100

    added components

---
+++ data.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: 'SMSL',
+  routes: [
+    '/',
+    '/about'
+  ]
+}
--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+var Routes = require('./Routes.jsx');
+
 var SmslMainPage = React.createClass({
 	render: function(){
 		return (
 			<div className="smslMain">
 				<h1>Welcome Smsl</h1>
+				<Routes />
 			</div>
 		);
 	}
